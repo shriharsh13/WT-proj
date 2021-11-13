88680912f02b51ef67f157c90cbb62f6d9449c7c
shriharsh13 <60087295+shriharsh13@users.noreply.github.com>
2021-11-13 18:11:53 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WT-proj';
+  title = 'angularbootstrap';
+   ngOnInit() {
+                //Toggle Click Function
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  }
+  
 }
